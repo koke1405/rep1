f898276ce54743808c952a8c3aafaacb2cdf9f8e
koke1405 <72536101+koke1405@users.noreply.github.com>
2020-10-07 22:23:42 -0500

    Add files via upload

---
+++ App.py
@@ -0,0 +1,71 @@
+from flask import Flask,render_template,request,redirect,url_for,flash
+from flask_mysqldb import MySQL
+
+app=Flask(__name__)
+
+#Mysql Connection
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']=''
+app.config['MYSQL_DB']='flaskcontacts'
+mysql=MySQL(app)
+
+#session
+app.secret_key='mysecretkey'
+
+@app.route('/')
+def index():
+    cur=mysql.connection.cursor()
+    cur.execute('select * from contacts')
+    data=cur.fetchall()
+    print(data)
+    return render_template('index.html',contacts=data)
+
+@app.route('/add_contact',methods=['POST'])
+def add_contact():
+    if request.method=='POST':
+        fullname=request.form['fullname']
+        phone=request.form['phone']
+        email=request.form['email']
+        cur=mysql.connection.cursor()
+        cur.execute('INSERT INTO CONTACTS (fullname,phone,email) VALUES (%s,%s,%s)',(fullname,phone,email))
+        mysql.connection.commit()
+        flash('Contact Added')
+        return redirect(url_for('index'))
+
+@app.route('/edit/<id>')
+def get_contact(id):
+    cur=mysql.connection.cursor()
+    cur.execute("select * from contacts where id=%s",id)
+    data=cur.fetchall()
+    return render_template('edit-contact.html',contact=data[0])
+
+@app.route('/update/<id>',methods=['POST'])
+def update_contact(id):
+    if request.method=='POST':
+        fullname=request.form['fullname']
+        phone=request.form['phone']
+        email=request.form['email']
+        cur=mysql.connection.cursor()
+        cur.execute("""
+            update contacts set
+            fullname=%s
+            ,phone=%s
+            ,email=%s
+            where id=%s
+        """,(fullname,phone,email,id))
+        mysql.connection.commit()
+        flash('Contacto Actualizado')
+        return redirect(url_for('index'))
+
+@app.route('/delete/<string:id>')
+def delete_contact(id):
+    cur=mysql.connection.cursor()
+    cur.execute('delete from contacts where id=%s',id)
+    mysql.connection.commit()
+    flash('Contacto Deleted')
+    return redirect(url_for('index'))
+
+
+if __name__=='__main__':
+ app.run(port=3000,debug=True)
